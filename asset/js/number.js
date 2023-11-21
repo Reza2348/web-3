281e7cceeb9dbce114cd9535c953d52963f3e096
Amier.reza majidaie <146933133+Reza2348@users.noreply.github.com>
2023-11-21 13:02:35 +0330

    Add files via upload

---
+++ asset/js/number.js
@@ -0,0 +1,19 @@
+      num1 = Number(prompt("please enter num1 : "));
+      num2 = Number(prompt("please enter num2 : "));
+      console.log(num1 + "+" + num2 + "=" + (num1 + num2));
+
+      num3 = Number(prompt("please enter num3 : "));
+      num4 = Number(prompt("please enter num4 : "));
+      console.log(num3 + "/" + num4 + "=" + num3 / num4);
+
+      num5 = Number(prompt("please enter num5 : "));
+      num6 = Number(prompt("please enter num6 : "));
+      console.log(num5 + "*" + num6 + "=" + num5 * num6);
+
+      num7 = Number(prompt("please enter num7 : "));
+      num8 = Number(prompt("please enter num8 : "));
+      console.log(num7 + "%" + num8 + "=" + (num7 % num8));
+
+      num9 = Number(prompt("please enter num9 : "));
+      num10 = Number(prompt("please enter num10 : "));
+      console.log(num9 + "**" + num10 + "=" + num9 ** num10);
