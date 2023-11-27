80682140b66f31ecd972d3929f25c5faef924c1b
Amier.reza majidaie <146933133+Reza2348@users.noreply.github.com>
2023-11-27 20:33:43 +0330

    Add files via upload

---
+++ switch,if/switch.js
@@ -0,0 +1,51 @@
+var weekDay = 2;
+switch (weekDay) {
+  case 1:
+    console.log("Sandy");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  default:
+    console.log("Please enter numbers 1 to 6");
+}
+
+var MonthsOfTheYear = 5;
+switch (MonthsOfTheYear) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("May");
+    break;
+  case 5:
+    console.log("June");
+    break;
+  case 6:
+    console.log("July");
+    break;
+  case 7:
+    console.log("August");
+    break;
+  case 8:
+    console.log("September");
+    break;
+}
