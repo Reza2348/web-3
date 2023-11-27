f36dce7931ab06aa9ef22d41e771ec28e122dd33
Amier.reza majidaie <146933133+Reza2348@users.noreply.github.com>
2023-11-27 20:35:34 +0330

    Add files via upload

---
+++ number,Array/Array.js
@@ -0,0 +1,33 @@
+      var food = ["ali", "reza", "ahmad"];
+      console.log(food);
+      food.push("amier");
+      console.log(food);
+      // Adds new elements to the end of an array, and returns the new length
+
+
+      var fruits = ["apple", "banana", "mango"];
+      console.log(fruits);
+      console.log(fruits.length);
+      //Sets or returns the number of elements in an array
+
+      var Nature= ['coconut','Pineapple','Tree','Garden'];
+      console.log(Nature);
+      console.log(Nature.toString());
+      //Converts an array to a string, and returns the result
+
+      var Book= ['Novel','story','Report','Poems','favor'];
+      console.log(Book);
+      console.log(Book.slice(3, 5));
+    //	Selects a part of an array, and returns the new array
+
+    const arr1 = ['Cecilie','lone'];
+    const arr2 = ['emill','tobias','linus'];
+    const children = arr1.concat(arr2);
+    // 	Joins arrays and returns an array with the joined arrays
+
+    var fruits = ['coconut','mango','Pineapple','Lemon','banana'];
+    fruits.sort();
+    console.log(fruits);
+    // Sorts the elements of an array
+
+    
