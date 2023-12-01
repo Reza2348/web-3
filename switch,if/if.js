507b92d688e82df53ad0116e78f6a2e2a5c1abd1
Amier.reza majidaie <146933133+Reza2348@users.noreply.github.com>
2023-12-01 10:11:31 +0330

    Update if.js

--- switch,if/if.js
+++ switch,if/if.js
@@ -1,23 +1,23 @@
-// var userName = prompt("لطفا نام کاربری خود را وارد کنید:");
-// var passWord = prompt("لطفا رمز عبور خود را وارد کنید");
-// userName = "Admin";
-// passWord = 123;
-// if (userName == "Admin" && passWord == "123") {
-//   console.log("مدیر وارد سیستم شده");
-// } else if (userName != "reza" && passWord == "s2314") {
-//   console.log("کارمند وارد سیستم شد");
-// }
+ var userName = prompt("لطفا نام کاربری خود را وارد کنید:");
+ var passWord = prompt("لطفا رمز عبور خود را وارد کنید");
+ userName = "Admin";
+ passWord = 123;
+ if (userName == "Admin" && passWord == "123") {
+   console.log("مدیر وارد سیستم شده");
+ } else if (userName != "reza" && passWord == "s2314") {
+   console.log("کارمند وارد سیستم شد");
+ }
 
-// var Weight = Number(prompt("لطفان وزن خود را وارد کنید"));
-// if (Weight <= 10 || Weight <= 30) {
-//   console.log("وزن شما کم است");
-// } else if (Weight <= 30 || Weight <= 50) {
-//   console.log("وزن شما مناسب است");
-// } else if (Weight <= 50 || Weight <= 100) {
-//   console.log("وزن شما خیلی زیاد است");
-// } else {
-//   console.log("لطفان وزن خود را بین 0تا100 وارد کنید");
-// }
+ var Weight = Number(prompt("لطفان وزن خود را وارد کنید"));
+ if (Weight <= 10 || Weight <= 30) {
+   console.log("وزن شما کم است");
+ } else if (Weight <= 30 || Weight <= 50) {
+   console.log("وزن شما مناسب است");
+ } else if (Weight <= 50 || Weight <= 100) {
+   console.log("وزن شما خیلی زیاد است");
+ } else {
+   console.log("لطفان وزن خود را بین 0تا100 وارد کنید");
+ }
 
 var time = Number(prompt("لطفان ساعت ورود خود به شرکت را تایپ کنید"));
 if (time <= 6 || time <= 10) {
