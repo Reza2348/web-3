88e6dc8fe980288b330a53f78956217bab162c58
Amier Reza majidaie <146933133+Reza2348@users.noreply.github.com>
2023-12-19 11:26:11 +0330

    Update while.js

--- while,function/while.js
+++ while,function/while.js
@@ -1,7 +1,7 @@
 // 3 examples of the while loop
 
-// let age = -1;
-// while (age < 0 || age > 100) {
-//   age = Number(prompt("plrase entet your age : "));
-// }
-// console.log("yor age is " + age);
+ let age = -1;
+ while (age < 0 || age > 100) {
+   age = Number(prompt("plrase entet your age : "));
+ }
+ console.log("yor age is " + age);
